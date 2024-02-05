1d1a9be007c6f15fe81065c03ff120fa63f56f3d
Riley-Eidson <153471738+Riley-Eidson@users.noreply.github.com>
2024-02-04 21:28:52 -0500

    Update dog.js

--- dog.js
+++ dog.js
@@ -6,7 +6,7 @@ import {
   
   const dogElem = document.querySelector("[data-dog]")
   const JUMP_SPEED = 0.45
-  const GRAVITY = 0.0115
+  const GRAVITY = 0.00015
   const DOG_FRAME_COUNT = 2
   const FRAME_TIME = 100
   
